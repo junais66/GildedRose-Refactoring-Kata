38f0d85db9bf83b3a78bb1ab3b597bca9f455f26
junais <junais66@icloud.com>
2024-07-04 23:43:40 +0400

    Refactor: Added Initial tests

--- GildedRoseTests/GildedRoseTest.cs
+++ GildedRoseTests/GildedRoseTest.cs
@@ -7,11 +7,67 @@ namespace GildedRoseTests;
 public class GildedRoseTest
 {
     [Fact]
-    public void foo()
+    //At the end of each day, our system lowers the value of sellin for every item
+    public void UpdateQuality_Should_ReduceSellInTo4WhenInitializeAs5()
     {
-        IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-        GildedRose app = new GildedRose(Items);
-        app.UpdateQuality();
-        Assert.Equal("fixme", Items[0].Name);
+        //Given the item has sell in of 5
+        var item = CreateItem("Foo", 5, 5);
+        var APP = CreateGildedRose(item);
+
+        //When the quality update
+        APP.UpdateQuality();
+
+        // then the sellIn is 4
+        Assert.Equal(4, item.SellIn);
     }
+    [Fact]
+    //At the end of each day, our system lowers the value of Quality for every item
+    public void UpdateQuality_Should_ReduceQualityTo4WhenInitializeAs5()
+    {
+
+        //Given the item has qualit of 5
+        var item = CreateItem("Foo", 5, 5);
+        var APP = CreateGildedRose(item);
+
+        APP.UpdateQuality();
+
+        // then the quality value is 4
+        Assert.Equal(4, item.Quality);
+    }
+
+    [Fact]
+    //Once the sell-by date has passed, Quality degrades twice as fast
+    public void UpdateQuality_Should_ReduceQualityBy2WhenSellInIsLessThanOrEqualToZero()
+    {
+        // Given the sell-by date has passed
+        var zeroSellInItem = CreateItem("foo", 3, -1);
+        var APP = CreateGildedRose(zeroSellInItem);
+
+        APP.UpdateQuality();
+
+        // Then the quality degraded twice as fast
+        Assert.Equal(1, zeroSellInItem.Quality);
+    }
+
+    [Fact]
+    //The Quality of an item is never negative
+    public void UpdateQuality_Should_KeepZeroQuality_When_QualityIsAlreadyZero()
+    {
+        // Given the quality of an item is 0
+        var zeroQualityItem = CreateItem("foo", 0, 5);
+        var APP = CreateGildedRose(zeroQualityItem);
+
+        APP.UpdateQuality();
+
+        // Then the quality is still o
+        Assert.Equal(0, zeroQualityItem.Quality);
+    }
+
+    //Helper functions
+    public static Item CreateItem(string name, int quality, int sellIn) =>
+    new Item { Name = name, Quality = quality, SellIn = sellIn };
+
+    private static GildedRose CreateGildedRose(Item newItem) =>
+        new GildedRose(new List<Item> { newItem });
+   
 }
